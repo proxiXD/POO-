a13d5a40708013a044746f27c3095a73f76afdc0
JULiAN_33 <julian.xplay33@gmail.com>
2026-05-11 20:43:48 -0500

    Enhance Arma class with tipo and damage handling

--- Proyecto inventario/models/arma.py
+++ Proyecto inventario/models/arma.py
@@ -2,18 +2,27 @@ from abstract.item_base import Item
 from interfaces.equipable import Equipable
 
 class Arma(Item, Equipable):
-    def __init__(self, nombre, peso, dano):
+    def __init__(self, nombre, peso, dano, tipo):
         super().__init__(nombre, peso)
         self._dano = dano
+        self._tipo = tipo
         self._equipada = False
 
-    def usar(self):
-        print(f"Atacas con {self._nombre} y haces {self._dano} de daño")
+    @property
+    def dano(self):
+        return self._dano
 
-    def equipar(self):
+    @property
+    def tipo(self):
+        return self._tipo
+
+    def usar(self, objetivo):
+        print(f"Atacas con {self._nombre}")
+        objetivo.recibir_dano(self._dano)
+
+    def equipar(self, jugador):
         self._equipada = True
-        print(f"{self._nombre} equipada")
+        jugador.agregar_equipo(self)
 
-    def desequipar(self):
+    def desequipar(self, jugador):
         self._equipada = False
-        print(f"{self._nombre} desequipada")
